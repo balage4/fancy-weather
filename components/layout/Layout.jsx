7bff172078d5dfdab382e6c34f45b20912a246ae
balage4 <balage4@hotmail.com>
2023-01-18 08:36:17 +0100

    minor change layout

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,5 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Navigation from '../navigation/Navigation';
+
 const Layout = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <Container>
+      <Navigation />
+      <Container>
+        <Row>
+          <Col>1</Col>
+          <Col>2</Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 };
 
 export default Layout;
