a0d46567b677c75d63bb3cc97583fd6e40aa115b
balage4 <balage4@hotmail.com>
2023-01-18 09:30:56 +0100

    add CitiesForm to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import { useSelector } from 'react-redux';
+import Container from 'react-bootstrap/Container';
+import CitiesForm from '../components/form/CitiesForm';
 
 const Home = () => {
   const cities = useSelector((state) => state.cities.value);
   console.log(cities);
-  return <>Home</>;
+  return (
+    <Container>
+      <CitiesForm />
+    </Container>
+  );
 };
 
 export default Home;
