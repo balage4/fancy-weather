659ae6c8e6c9e73ef9cfd1486a3d20536d9f37d1
balage4 <balage4@hotmail.com>
2023-01-18 12:57:10 +0100

    add sidebar basics

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,3 +1,16 @@
-const Sidebar = () => <>Sidebar</>;
+import { Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+
+const Sidebar = () => {
+  const cities = useSelector((state) => state.cities.value);
+
+  return (
+    <Container>
+      {cities.map((city, index) => (
+        <p key={`sidebarList_${index}`}>{city.name}</p>
+      ))}
+    </Container>
+  );
+};
 
 export default Sidebar;
