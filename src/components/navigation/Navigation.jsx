f2eaa1cd09230758a1c1bd8dc3c6ba6a75c2f4b0
balage4 <balage4@hotmail.com>
2023-01-18 13:38:16 +0100

    update Nav

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,13 +1,7 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import routes from '../../common/routes/routes';
 
 const Navigation = () => {
-  const navMenus = [
-    {
-      name: 'Home',
-      href: '/',
-    },
-  ];
-
   return (
     <Navbar bg="light" expand="lg" sticky="top">
       <Container>
@@ -15,11 +9,13 @@ const Navigation = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            {navMenus.map((navMenu, index) => (
-              <Nav.Link key={`navmenu_${index}`} href={navMenu.href}>
-                {navMenu.name}
-              </Nav.Link>
-            ))}
+            {routes
+              .filter((route) => route.topMenu)
+              .map((navMenu, index) => (
+                <Nav.Link key={`navmenu_${index}`} href={navMenu.path}>
+                  {navMenu.name}
+                </Nav.Link>
+              ))}
           </Nav>
         </Navbar.Collapse>
       </Container>
