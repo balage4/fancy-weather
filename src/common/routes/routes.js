b27006bd9fde55c8c6c65da4c9465ee54de909fa
balage4 <balage4@hotmail.com>
2023-01-18 13:37:14 +0100

    update routes object

--- src/common/routes/routes.js
+++ src/common/routes/routes.js
@@ -1,10 +1,21 @@
+import City from "../../pages/City";
 import Home from "../../pages/Home";
 
 const routes = [
   {
     path: '/',
-    element: Home
+    name: 'Home',
+    element: Home,
+    topMenu: true
+  },
+  {
+    path: '/city/:cityname',
+    name: 'City',
+    element: City,
+    topMenu: false
   }
 ];
+
+
 
 export default routes;
