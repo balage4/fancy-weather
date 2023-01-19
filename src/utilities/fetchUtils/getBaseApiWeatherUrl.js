e459da9800c5e31a71e939151d4543e00da1b333
balage4 <balage4@hotmail.com>
2023-01-19 14:57:49 +0100

    tweak api handler

--- src/utilities/fetchUtils/getBaseApiWeatherUrl.js
+++ src/utilities/fetchUtils/getBaseApiWeatherUrl.js
@@ -3,9 +3,24 @@ const env = import.meta.env;
 const {
   VITE_WEATHER_API_KEY,
   VITE_WEATHER_API_PROTOCOL,
+  VITE_WEATHER_API_API,
   VITE_WEATHER_API_HOST,
+  VITE_WEATHER_API_WEATHER_ROUTE,
+  VITE_WEATHER_API_ICON_ROUTE
 } = env;
 
-const getBaseApiWeatherUrl = (cityName) => `${VITE_WEATHER_API_PROTOCOL}://${VITE_WEATHER_API_HOST}?q=${cityName}&appid=${VITE_WEATHER_API_KEY}aaa`;
+const lang = 'hu';
 
-export default getBaseApiWeatherUrl;
+const addLangQuery = (lang) => {
+  if (lang) return `&lang=${lang}`
+  return '';
+}
+const baseIconUrl = `${VITE_WEATHER_API_PROTOCOL}://${VITE_WEATHER_API_HOST}/${VITE_WEATHER_API_ICON_ROUTE}`;
+const getBaseApiWeatherUrl = (cityName) => `${VITE_WEATHER_API_PROTOCOL}://${VITE_WEATHER_API_API}.${VITE_WEATHER_API_HOST}/${VITE_WEATHER_API_WEATHER_ROUTE}?q=${cityName}${addLangQuery(lang)}&appid=${VITE_WEATHER_API_KEY}`;
+
+const apiURl = {
+  baseIconUrl,
+  baseApiUrl: getBaseApiWeatherUrl,
+}
+
+export default apiURl;
