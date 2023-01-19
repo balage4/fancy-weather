dfe5b46a23abd7e626efb7d0ee38e3270764edde
balage4 <balage4@hotmail.com>
2023-01-19 14:04:51 +0100

    add CityWeather class

---
+++ src/common/classes/CityWeather.js
@@ -0,0 +1,31 @@
+import roundToFloat from "../../utilities/roundToFloat";
+import twoDigits from "../../utilities/twoDigits";
+
+export default class CityWeather {
+
+  rawData;
+
+  constructor(rawData) {
+    this.data = rawData;
+  }
+
+  getTemperature() {
+    const kelvinLevel = 273.15;
+    return roundToFloat(Number(this.data.main.temp) - kelvinLevel);
+  }
+
+  getWindSpeed() {
+    const kmhExchange = 3.6;
+    return roundToFloat(Number(this.data.wind.speed) * 3.6);
+  }
+  getHumidity() {
+    return this.data.main.humidity;
+  }
+  getWeatherString() {
+    return `${this.data.weather[0].main}, ${this.data.weather[0].description}`
+  }
+  getCurrentTime() {
+    const d = new Date();
+    return `${twoDigits(d.getHours())}:${twoDigits(d.getMinutes())}`;
+  }
+}
