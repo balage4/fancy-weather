18bfa9304f7015f573d6147b0a077b858eb1e891
balage4 <balage4@hotmail.com>
2023-01-21 21:30:57 +0100

    update fetch methods

--- src/utilities/fetchUtils/fetchWeather.js
+++ src/utilities/fetchUtils/fetchWeather.js
@@ -1,10 +1,19 @@
 import throwStatusError from './throwStatusError';
 import errorMessages from '../../common/messages/errorMessages';
+import apiUrl from './apiUrl';
 
 const fetchWeather = async (weatherUrl) => {
+
   const weatherResponse = await fetch(weatherUrl);
   throwStatusError(weatherResponse.status, weatherResponse.message || errorMessages.fetchError);
   return weatherResponse.json();
+}
+
+
+export const fetchAllWeatherData = async (cityName) => {
+  const weather = fetch(apiUrl.currentWeatherUrl(cityName));
+  const forecast = fetch(apiUrl.forecastUrl(cityName));
+  return await Promise.all([weather, forecast]).then(res => Promise.all(res.map(res => res.json())));
 }
 
 export default fetchWeather;
