d7bcd02a63d69e7f5c2d7ce2aa3aeed21f637343
balage4 <balage4@hotmail.com>
2023-01-22 10:57:19 +0100

    styling temp chart

--- src/components/foreacast/TemperatureChart.jsx
+++ src/components/foreacast/TemperatureChart.jsx
@@ -5,16 +5,27 @@ import {
   CartesianGrid,
   Line,
   ResponsiveContainer,
+  Tooltip,
 } from 'recharts';
 
 const TemperatureChart = ({ temperaturesData }) => (
   <ResponsiveContainer height={300}>
-    <LineChart data={temperaturesData}>
+    <LineChart data={temperaturesData} domain={['auto', 'auto']}>
       <XAxis dataKey="name" />
-      <YAxis />
+      <YAxis /> <Tooltip />
       <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-      <Line type="monotone" dataKey="minTemp" stroke="#8884d8" />
-      <Line type="monotone" dataKey="maxTemp" stroke="#82ca9d" />
+      <Line
+        type="monotone"
+        dataKey="minTemp"
+        stroke="#334cd6"
+        strokeWidth={2}
+      />
+      <Line
+        type="monotone"
+        dataKey="maxTemp"
+        stroke="#fc840c"
+        strokeWidth={2}
+      />
     </LineChart>
   </ResponsiveContainer>
 );
