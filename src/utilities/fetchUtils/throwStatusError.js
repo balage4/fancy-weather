2131c56758a866ff475ff4a08b4fb52875309abc
balage4 <balage4@hotmail.com>
2023-01-24 12:59:29 +0100

    fix throw status error fn

--- src/utilities/fetchUtils/throwStatusError.js
+++ src/utilities/fetchUtils/throwStatusError.js
@@ -1,5 +1,8 @@
-const throwStatusError = (statusCode, backendMessage) => {
-  if (statusCode < 200 || statusCode >= 300) throw new Error(backendMessage || 'Something went wrong!');
+import errorMessages from "../../common/messages/errorMessages";
+
+const throwStatusError = (statusCode, responseMessage) => {
+  if (statusCode < 200 || statusCode >= 300) throw new Error(responseMessage || errorMessages.fetchError);
+  return;
 }
 
 export default throwStatusError;
