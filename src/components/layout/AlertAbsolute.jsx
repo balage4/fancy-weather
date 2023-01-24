42ff707753a6a2c64090cfc91e556a28ce122be0
balage4 <balage4@hotmail.com>
2023-01-24 13:01:18 +0100

    add Alert comp

---
+++ src/components/layout/AlertAbsolute.jsx
@@ -0,0 +1,11 @@
+import { Alert } from 'react-bootstrap';
+
+const AlertAbsolute = ({ message }) => {
+  return (
+    <Alert style={{ position: 'absolute', top: '40%', left: '40%' }}>
+      <Alert variant="danger">{message}</Alert>
+    </Alert>
+  );
+};
+
+export default AlertAbsolute;
